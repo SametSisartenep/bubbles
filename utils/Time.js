fd9d65f095f2447d552107a5c309b3d665ff3c7a
SametSisartenep <krashbuffer@gmail.com>
2014-09-18 22:19:54 +0200

    Utils file decompressed on 'utils' for a better study thereof.

---
+++ utils/Time.js
@@ -0,0 +1,28 @@
+
+function Time () { this.now = new Date(); }
+
+Time.prototype = {
+	day: function () {
+		var d = this.now.getDate();
+		return d < 10 ? '0' + d : d;
+	},
+	month: function () {
+		var m = this.now.getMonth();
+		return m < 10 ? '0' + m : m;
+	},
+	year: function () {
+		return this.now.getFullYear();
+	},
+	hour: function () {
+		var hh = this.now.getHours();
+		return hh < 10 ? '0' + hh : hh;
+	},
+	minute: function () {
+		var mm = this.now.getMinutes();
+		return mm < 10 ? '0' + mm : mm;
+	},
+	second: function () {
+		var ss = this.now.getSeconds();
+		return ss < 10 ? '0' + ss : ss;
+	}
+};
