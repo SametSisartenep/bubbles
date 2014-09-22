934dcece4e13e6fdb05597d78fd3d75130a8c247
SametSisartenep <krashbuffer@gmail.com>
2014-09-22 20:09:48 +0200

    fonts added.

--- www/scripts/ui.js
+++ www/scripts/ui.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
 
+	var prevColor;
+
 	$('a').mouseenter(function() {
+		prevColor = $(this).css("color");
 		$(this).css("color", "#FFFB32");
 	}).mouseleave(function () {
-		$(this).css("color", "#FFF");
+		$(this).css("color", prevColor);
 	});
 
 	$('li').mouseenter(function () {
+		prevColor = $(this).css("color");
 		$(this).css("color", "#FFFF00");
 	}).mouseleave(function () {
-		$(this).css("color", "#FFF");
+		$(this).css("color", prevColor);
 	});
 
 });
