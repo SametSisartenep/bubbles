e4907f7a7d7f183ba89bd138ee050e0cc3f72a3f
SametSisartenep <krashbuffer@gmail.com>
2014-09-27 21:45:47 +0200

    3D homepage prototype created. background music added. json-model deleted, now embedded within 3core.js

--- www/res/scripts/3core.js
+++ www/res/scripts/3core.js
@@ -1,85 +1,151 @@
 $(document).ready(function() {
 
-	function loadModel() {
-  		var loader = new THREE.JSONLoader();
-  		loader.load("res/models/monkey.js",function(model) {
-  			var monkeyMaterial = new THREE.MeshLambertMaterial({color: 0xFF00FF});
+	$('#mute img').click(function () {
+		if ($('#mute img').hasClass('clicked'))
+		{
+			$('#mute img').attr('src', 'res/images/mute-sound-light.png');
+			$('#mute img').removeClass('clicked');
+			document.getElementById('home-music').play();
+		}
+		else
+		{
+			$('#mute img').attr('src', 'res/images/play-sound-light.png');
+			$('#mute img').addClass('clicked');
+			document.getElementById('home-music').pause();
+		}
+	});
 
-    		var monkey = new THREE.Mesh(model, monkeyMaterial);
-    		monkey.position.x = 0;
-    		monkey.position.y = 0;
-    		monkey.position.z = 0;
-    		monkey.scale = new THREE.Vector3(3, 3, 3);
+	init();
 
-    		scene.add(monkey);
-  		});
+	// global variables
+	var renderer,
+		scene,
+		camera,
+		control,
+		stats;
+
+
+	/**
+	* Initializes the scene, camera and objects. Called when the window is
+	* loaded by using window.onload (see below)
+	*/
+	function init() {
+
+		// create a scene, that will hold all our elements such as objects, cameras and lights.
+		scene = new THREE.Scene();
+
+		// create a camera, which defines where we're looking at.
+		camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+		// create a render, sets the background color and the size
+		renderer = new THREE.WebGLRenderer();
+		renderer.setClearColor(0x000000, 1.0);
+		renderer.setSize(window.innerWidth, window.innerHeight);
+		renderer.shadowMapEnabled = true;
+
+		// create the ground plane
+		var planeGeometry = new THREE.PlaneGeometry(20, 20);
+		var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
+		var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+		plane.receiveShadow = true;
+
+		// rotate and position the plane
+		plane.rotation.x = -0.5 * Math.PI;
+		plane.position.x = 0;
+		plane.position.y = -2;
+		plane.position.z = 0;
+
+
+		// position and point the camera to the center of the scene
+		camera.position.x = 3;
+		camera.position.y = 3;
+		camera.position.z = 3;
+		camera.lookAt(scene.position);
+
+
+		// setup the control object for the control gui
+		control = new function() {
+			this.rotationSpeed = 0.005;
+		};
+
+		// add extras
+		addControlGui(control);
+		addStatsObject();
+
+		loadModel();
+
+
+		// add the output of the renderer to the html element
+		document.body.appendChild(renderer.domElement);
+
+		// call the render function, after the first render, interval is determined
+		// by requestAnimationFrame
+		render();
 	}
 
-	var scene = new THREE.Scene();
+	function loadModel() {
+		var loader = new THREE.JSONLoader();
+		loader.load("res/models/monkey.js",
+		function(model) {
+			var material = new THREE.MeshNormalMaterial();
 
-	var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+			var mesh = new THREE.Mesh(model, material);
+			mesh.translateY(-0.5);
+			mesh.scale = new THREE.Vector3(3,3,3);
 
-	//var renderer = new THREE.CanvasRenderer();
-	var renderer = new THREE.WebGLRenderer();
-	renderer.setClearColor(0xEEEEEE, 1.0);
-	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.shadowMapEnabled = true;
+			scene.add(mesh);
+		});
+	}
 
-	//var axes = new THREE.AxisHelper(20);
-	//scene.add(axes);
 
-	var planeGeometry = new THREE.PlaneGeometry(60, 20);
-	var planeMaterial = new THREE.MeshLambertMaterial({color: 0xcccccc});
-	var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+	function addControlGui(controlObject) {
+		var gui = new dat.GUI();
+		gui.add(controlObject, 'rotationSpeed', -0.01, 0.01);
+	}
 
-	plane.rotation.x = -0.5 * Math.PI;
+	function addStatsObject() {
+		stats = new Stats();
+		stats.setMode(0);
 
-	plane.position.x = 15;
-	plane.position.y = 0;
-	plane.position.z = 0;
+		stats.domElement.style.position = 'absolute';
+		stats.domElement.style.left = '0px';
+		stats.domElement.style.top = '0px';
 
-	plane.receiveShadow = true;
+		document.body.appendChild( stats.domElement );
+	}
 
-	scene.add(plane);
 
-	var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-	var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000, wireframe: false});
-	var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+	/**
+	 * Called when the scene needs to be rendered. Delegates to requestAnimationFrame
+	 * for future renders
+	 */
+	function render() {
+		// update the camera
+		var rotSpeed = control.rotationSpeed;
+		camera.position.x = camera.position.x * Math.cos(rotSpeed) + camera.position.z * Math.sin(rotSpeed);
+		camera.position.z = camera.position.z * Math.cos(rotSpeed) - camera.position.x * Math.sin(rotSpeed);
+		camera.lookAt(scene.position);
 
-	cube.position.x = -4;
-	cube.position.y = 3;
-	cube.position.z = 0;
+		// update stats
+		stats.update();
 
-	cube.castShadow = true;
+		// and render the scene
+		renderer.render(scene, camera);
 
-	scene.add(cube);
+		// render using requestAnimationFrame
+		requestAnimationFrame(render);
+	}
 
-	var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-	var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff, wireframe: false});
-	var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
 
-	sphere.position.x = 20;
-	sphere.position.y = 4;
-	sphere.position.z = 2;
-
-	sphere.castShadow = true;
-
-	scene.add(sphere);
-
-	loadModel();
-
-	var spotLight = new THREE.SpotLight(0xffffff);
-	spotLight.position.set(-40, 60, -10);
-
-	spotLight.castShadow = true;
-
-	scene.add(spotLight);
-
-	camera.position.x = -30;
-	camera.position.y = 40;
-	camera.position.z = 30;
-	camera.lookAt(scene.position);
-
-	$('#WebGL-output').append(renderer.domElement);
-	renderer.render(scene, camera);
+	/**
+	 * Function handles the resize event. This make sure the camera and the renderer
+	 * are updated at the correct moment.
+	 */
+	function handleResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+	// calls the handleResize function when the window is resized
+	window.addEventListener('resize', handleResize, false);
 });
