0b2bf4bdc5672cf955e5b2e3f0f340aaa0d1e10b
SametSisartenep <krashbuffer@gmail.com>
2014-10-25 13:18:43 +0200

    Database module updated. REST functions' names correction.

--- db-interface.js
+++ db-interface.js
@@ -17,123 +17,114 @@ function checkArray ( object, callback ) {
     callback();
 }
 
-function connect () {
-  connection.connect(function ( error ) {
-    if ( error )
-    {
-      utils.logError('Error connecting: ' + error.stack);
-      return;
-    }
-
-    utils.log('MySQL connection established. id(' + connection.threadId + ')');
-  });
-}
-
-function query ( query ) {
-  connection.query(query, function ( error, rows, cols ) {
-    if ( error )
-    {
-      utils.logError('Query error: ' + error.stack);
-      return;
-    }
-
-    return { rows: rows, cols: cols };
-  });
-}
-
-function select ( table, fields, condition ) {
-  var SET = '';
-
-  if ( typeof fields === 'object' )
-  {
-    checkArray(fields, function () {
-      for ( var i = 0; i < fields.length; i++ )
+module.exports = exports = {
+  connect: function () {
+    connection.connect(function ( error ) {
+      if ( error )
       {
-        if ( i === fields.length - 1 )
-        {
-          SET += fields[i];
-        }
-        else
-        {
-          SET += fields[i] + ',';
-        } 
+        utils.logError('Error connecting: ' + error.stack);
+        return;
       }
+
+      utils.log('MySQL connection established. id(' + connection.threadId + ')');
     });
-  }
-  else if (typeof fields === 'string')
-  {
-    SET = fields;
-  }
-  else
-  {
-    utils.logError('Datatype not supported for querying.');
-    return;
-  }
-
-  return query('SELECT ' + SET + ' FROM ' + table + ' WHERE ' + condition);
-}
-
-function update ( table, fields, values, condition ) {
-  var SET = '';
-
-  if ( typeof fields === 'object' && typeof values === 'object' )
-  {
-    // If "fields" is 'Array', then check "values". If it's too, execute this lambda.
-    checkArray(fields, checkArray(values, function () {
-      if ( fields.length === values.length )
+  },
+  query: function ( query ) {
+    connection.query(query, function ( error, rows, cols ) {
+      if ( error )
       {
+        utils.logError('Query error: ' + error.stack);
+        return;
+      }
+
+      return { rows: rows, cols: cols };
+    });
+  },
+  SELECT: function ( table, fields, condition ) {
+    var SET = '';
+
+    if ( typeof fields === 'object' )
+    {
+      checkArray(fields, function () {
         for ( var i = 0; i < fields.length; i++ )
         {
           if ( i === fields.length - 1 )
           {
-            SET += fields[i] + '=' + values[i];
+            SET += fields[i];
           }
           else
           {
-            SET += fields[i] + '=' + values[i] + ',';
+            SET += fields[i] + ',';
+          } 
+        }
+      });
+    }
+    else if (typeof fields === 'string')
+    {
+      SET = fields;
+    }
+    else
+    {
+      utils.logError('Datatype not supported for querying.');
+      return;
+    }
+
+    return query('SELECT ' + SET + ' FROM ' + table + ' WHERE ' + condition);
+  },
+  UPDATE: function ( table, fields, values, condition ) {
+    var SET = '';
+
+    if ( typeof fields === 'object' && typeof values === 'object' )
+    {
+      // If "fields" is 'Array', then check "values". If it's too, execute this lambda.
+      checkArray(fields, checkArray(values, function () {
+        if ( fields.length === values.length )
+        {
+          for ( var i = 0; i < fields.length; i++ )
+          {
+            if ( i === fields.length - 1 )
+            {
+              SET += fields[i] + '=' + values[i];
+            }
+            else
+            {
+              SET += fields[i] + '=' + values[i] + ',';
+            }
           }
         }
-      }
-      else
-      {
-        utils.logError('"fields" don\'t match "values".');
-        return;
-      }
-    }));
-  }
-  else if ( typeof fields === 'string' && typeof values === 'string' )
-  {
-    fields = fields.split(',');
-    values = values.split(',');
-
-    for ( var i = 0; i < fields.length; i++ )
+        else
+        {
+          utils.logError('"fields" don\'t match "values".');
+          return;
+        }
+      }));
+    }
+    else if ( typeof fields === 'string' && typeof values === 'string' )
     {
-      if ( i === fields.length - 1 )
+      fields = fields.split(',');
+      values = values.split(',');
+
+      for ( var i = 0; i < fields.length; i++ )
       {
-        SET += fields[i] + '=' + values[i];
-      }
-      else
-      {
-        SET += fields[i] + '=' + values[i] + ',';
+        if ( i === fields.length - 1 )
+        {
+          SET += fields[i] + '=' + values[i];
+        }
+        else
+        {
+          SET += fields[i] + '=' + values[i] + ',';
+        }
       }
     }
+    else
+    {
+      utils.logError('Datatypes not supported for querying.');
+      return;
+    }
+
+    query('UPDATE ' + table + ' SET ' + SET + ' WHERE ' + condition);
+  },
+  DELETE: function ( table, condition ) {
+    query('DELETE FROM ' + table + ' WHERE ' + condition);
   }
-  else
-  {
-    utils.logError('Datatypes not supported for querying.');
-    return;
-  }
-
-  query('UPDATE ' + table + ' SET ' + SET + ' WHERE ' + condition);
-}
-
-function delete ( table, condition ) {
-  query('DELETE FROM ' + table + ' WHERE ' + condition);
-}
-
-module.exports = exports = {
-  connect: connect(),
-  select: select(),
-  update: update(),
-  delete: delete()
 };
